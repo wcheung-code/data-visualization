e7e693ec2c31dc6e79a7ab9a045759de5adf4933
Wilson Cheung <wcheung1991@gmail.com>
2017-03-26 20:00:28 -0400

    First edit of 12 Day Milestone Project

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template, request, redirect
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-  return redirect('/index')
+@app.route('/prices', methods=['POST'])
+def prices():
+    tsymbol = request.form['tsymbol']
+    r = requests.get('https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker='+tsymbol+'&qopts.columns=open,close,adj_open,adj_close&api_key=Y2Zioiyb9r16QRthEeyU')
+    json_object = r.json()
+    datalist = list(json_object[datatable][data])
+    return str(datalist)
+    #return render_template('prices.html')
+    #return redirect('/index')
 
 @app.route('/index')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507)
