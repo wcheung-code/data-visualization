cec850708395125385174c912dc5624c4459a642
Wilson Cheung <wcheung1991@gmail.com>
2017-03-26 21:28:06 -0400

    restarting

--- app.py
+++ app.py
@@ -1,21 +1,37 @@
-from flask import Flask, render_template, request, redirect
-import requests
+#from flask import Flask, render_template, request, redirect
+#import requests
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
-@app.route('/prices', methods=['POST'])
-def prices():
-    tsymbol = request.form['tsymbol']
-    r = requests.get('https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker='+tsymbol+'&qopts.columns=open,close,adj_open,adj_close&api_key=Y2Zioiyb9r16QRthEeyU')
-    json_object = r.json()
-    datalist = list(json_object[datatable][data])
-    return str(datalist)
+#@app.route('/prices', methods=['POST'])
+#def prices():
+#    tsymbol = request.form['tsymbol']
+#    r = requests.get('https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker='+tsymbol+'&qopts.columns=open,close,adj_open,adj_close&api_key=Y2Zioiyb9r16QRthEeyU')
+#    json_object = r.json()
+#    datalist = list(json_object[datatable][data])
+#    return str(datalist)
     #return render_template('prices.html')
     #return redirect('/index')
 
+#@app.route('/index')
+#def index():
+#    return render_template('index.html')
+
+#if __name__ == '__main__':
+#    app.run(host = '0.0.0.0', port = 33507)
+
+
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return redirect('/index')
+
 @app.route('/index')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 33507)
+    app.run(port=33507)
