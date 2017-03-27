fc3d95e42f53c8b8e023ecce0d68b2fbfe0cec21
Wilson Cheung <wcheung1991@gmail.com>
2017-03-26 21:59:45 -0400

    restarting1

--- app.py
+++ app.py
@@ -40,10 +40,9 @@ def prices():
     r = requests.get('https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker='+tsymbol1+'&qopts.columns=open,close,adj_open,adj_close&api_key=Y2Zioiyb9r16QRthEeyU')
     json_object = r.json()
     datalist = json_object['datatable']['data']
-    return str(datalist)
     #return render_template('prices.html')
     #return redirect('/index')
-#    return render_template('prices.html')
+    return render_template('prices.html', data=datalist)
 
 
 if __name__ == '__main__':
